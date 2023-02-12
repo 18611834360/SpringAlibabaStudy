67bf73b3656a5b8d1786c1fd731a99846c7e2ded
mac <mac@Mac.local>
2023-02-12 14:15:34 +0800

    gateway+nacos ok

---
+++ gateway/src/main/java/com/tulingxueyuan/GatewayApplication.java
@@ -0,0 +1,16 @@
+package com.tulingxueyuan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author:gaojinjun
+ * @date: 2023022023/2/12下午1:11
+ **/
+@SpringBootApplication
+public class GatewayApplication {
+    public static void main(String[] args) {
+
+        SpringApplication.run(GatewayApplication.class,args);
+    }
+}
